ce8766b008d2aa35106e55fc5bf4d5d414e4f780
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-03 16:57:24 -0300

    esboco: add pop-up settings

--- esboco/pagina_versiculos.js
+++ esboco/pagina_versiculos.js
@@ -1,10 +1,21 @@
 const buttonHideCard = document.querySelector(".hide-info");
 const verseInfoContent = document.getElementById("verse-info");
 
-buttonHideCard.addEventListener("click", () => {
-  if (verseInfoContent.style.display === "none") {
-    verseInfoContent.style.display = "flex";
+function eventHiden(element) {
+  console.log("tesddasadsa");
+  if (element.style.display === "none") {
+    element.style.display = "flex";
   } else {
-    verseInfoContent.style.display = "none";
+    element.style.display = "none";
   }
-});
+}
+
+buttonHideCard.addEventListener("click", () => eventHiden(verseInfoContent));
+
+// setings
+const buttonSettings = document.querySelectorAll(".pop-up-hiden");
+const setingsContent = document.getElementById("settings-popup");
+
+buttonSettings.forEach((element) =>
+  element.addEventListener("click", () => eventHiden(setingsContent))
+);
